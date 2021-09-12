b9ada73f96e2b4ee7cca2bfdd0f07f9837ac78be
hb_sanusi <sansbabshabs@gmail.com>
2021-09-11 22:22:33 -0400

    Add project files.

---
+++ JulianCalendarCpp/JulianCalendar.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+const int MAX_DAY= 31;
+const string MONTH_NAMES[] = { "Jan","Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+const int MONTH_LENGTH[] = { 31, 28, 31,30,31, 30, 31, 31, 30, 31, 30, 31 };
+int numberOfMonths = sizeof(MONTH_NAMES) / sizeof(MONTH_NAMES[0]);
+
+void setWidth() {
+	cout << setw(3) << setfill('0');
+}
+
+void displayTitle() {
+
+	cout << "Day" << " ";
+
+	for (int i = 0; i < numberOfMonths; i++) {
+		cout << MONTH_NAMES[i] << " ";
+	}
+
+	cout << "Day\n";
+}
+
+
+void displayNumbers() {
+	int currentAdd = 0;
+	int numOfDays = 0;
+	int currentMonth = 0;
+	for (numOfDays = 1; numOfDays <= MAX_DAY; numOfDays++) {
+		setWidth();
+		currentAdd = 0;
+		cout << numOfDays << " ";
+
+		for (currentMonth = 1; currentMonth <= (numberOfMonths); currentMonth++) {
+			setWidth();
+			if (numOfDays <= MONTH_LENGTH[currentMonth-1]) {
+				cout << numOfDays + currentAdd << " ";
+			}
+			else {
+				cout << "000" << " ";
+			}
+			currentAdd += MONTH_LENGTH[currentMonth-1];
+		}
+		
+		setWidth();
+		cout << numOfDays << endl;
+	}
+}
+
+int main() {
+	setWidth();
+	displayTitle();
+	setWidth();
+	displayNumbers();
+	return 0;
+}
---
+++ Source1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void JulianCal() {
+	const int MAXDAY = 31;
+	const String MONTHNAMES[] = { "Jan","Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+}
+
+void display() {
+
+}
+
+int main() {
+
+	return 0;
+}
